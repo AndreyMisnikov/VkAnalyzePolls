07e860d88f6a1e03b5187a8f29eb74e511c91c4b
Andrey <andrey9_5@mail.ru>
2016-12-08 01:05:35 +0300

    Initial
    Create WPF app

---
+++ Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkAnalyzePolls
+{
+    static class Settings
+    {
+        public static int ApplicationId
+        {
+            get { return Int32.Parse(ConfigurationSettings.AppSettings["appId"]); }
+        }
+
+        public static int UserId
+        {
+            get { return Int32.Parse(ConfigurationSettings.AppSettings["userId"]); }
+        }
+    }
+}
